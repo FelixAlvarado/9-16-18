e8fc4ad88ea070c3b170472054d865a2db26f55d
FelixAlvarado <felixalvarado653@gmail.com>
2018-09-16 19:01:56 -0700

    started lock safe problem

---
+++ practice.js
@@ -0,0 +1,16 @@
+var openLock = function(deadends, target, hash = false, count = 0, current = '0000') {
+    if(!hash){
+    deadend.forEach((end) =>{
+        hash[end] = true;
+    });
+    }
+    
+    for(let i = 0; i < current.length; i++){
+        let now = parseInt(current[i]);
+        let targ = parseInt(target[i]);
+        if (now != target){
+            
+            
+        }
+    }
+};
